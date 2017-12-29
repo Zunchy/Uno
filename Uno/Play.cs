40b4964db75570b08c6ea12238f05e26dacc82f6
Zach Waterbury <zwaterbury1289@gmail.com>
2017-12-28 22:07:17 -0600

    Put card properties into a Class and Cards into An Array.

--- Uno/Play.cs
+++ Uno/Play.cs
@@ -12,9 +12,46 @@ namespace Uno
 {
     public partial class Play : Form
     {
+
+        Card[] cardList = new Card[62];
+
+
+
         public Play()
         {
             InitializeComponent();
+
+            for(int i = 0; i < 13; i++)
+            {
+                cardList[i] = new Card(i, "Red");
+            }
+
+            for (int i = 13; i < 26; i++)
+            {
+                cardList[i] = new Card(i, "Yellow");
+            }
+    
+            for (int i = 26; i < 39; i++)
+            {
+                cardList[i] = new Card(i, "Green");
+            }
+             
+            for (int i = 39; i < 52; i++)
+            {
+                cardList[i] = new Card(i, "Blue");
+            }
+
+
+            for (int i = 52; i < 56; i++)
+            {
+                cardList[i] = new Card(10, "Wild");
+            }
+
+            for (int i = 56; i < 61; i++)
+            {
+                cardList[i] = new Card(10, "Draw4");
+            }
+
         }
     }
 }
---
+++ Uno/Resources/Card.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uno
+{
+    class Card
+    {
+        private int number;
+        private string color;
+        private string type;
+
+        public Card()
+        {
+        }
+
+        public Card(int num, string c)
+        {
+            number = num;
+            color = c;
+        }
+
+
+    }
+}
